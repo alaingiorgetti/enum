0cb261c1ebf5f25cca0d35194343862e47928933
agiorget <alain.giorgetti@univ-fcomte.fr>
2018-09-14 09:15:40 +0200

    distinct image and container names

--- Why3/gui.sh
+++ Why3/gui.sh
@@ -1,3 +1,3 @@
 # gui.sh
 docker create -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v ~/enum-master:/home/opam/app/enum-master --name="proof" proof
+  -v ~/enum-master:/home/opam/app/enum-master --name="proofctr" proofimg

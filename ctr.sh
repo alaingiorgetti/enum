486d7b8608f22ef4f9abc78047f0c20835332c9c
agiorget <alain.giorgetti@univ-fcomte.fr>
2019-06-13 15:04:57 +0200

    ENUM in C/ACSL and WhyML extractable into C

--- ctr.sh
+++ ctr.sh
@@ -6,11 +6,8 @@
 _pwd="$(pwd)"
 
 docker create -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
- -v ${_pwd}/bench:/home/opam/enum/bench \
  -v ${_pwd}/cACSL:/home/opam/enum/cACSL \
- -v ${_pwd}/cWhy3:/home/opam/enum/cWhy3 \
  -v ${_pwd}/OCaml:/home/opam/enum/OCaml \
  -v ${_pwd}/ptrWhy3:/home/opam/enum/ptrWhy3 \
- -v ${_pwd}/Why3:/home/opam/enum/Why3 \
  --name="enumctr" enum:latest
 

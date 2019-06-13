85895eb8a0391a56da65b6ad55f6fe25c1c14ebe
clotilde erard <clotilde.erard@edu.univ-fcomte.fr>
2019-06-13 20:40:46 +0200

    script for build a docker container

--- ctr.sh
+++ ctr.sh
@@ -7,7 +7,9 @@ _pwd="$(pwd)"
 
 docker create -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v ${_pwd}/cACSL:/home/opam/enum/cACSL \
+ -v ${_pwd}/cWhy3:/home/opam/enum/cWhy3 \
  -v ${_pwd}/OCaml:/home/opam/enum/OCaml \
  -v ${_pwd}/ptrWhy3:/home/opam/enum/ptrWhy3 \
+ -v ${_pwd}/Why3:/home/opam/enum/Why3 \
  --name="enumctr" enum:latest
 
--- ctrmac.sh
+++ ctrmac.sh
@@ -7,8 +7,10 @@ _pwd="$(pwd)"
 
 docker create -ti -e DISPLAY=docker.for.mac.host.internal:0 \
  -v ${_pwd}/cACSL:/home/opam/enum/cACSL \
+ -v ${_pwd}/cWhy3:/home/opam/enum/cWhy3 \
  -v ${_pwd}/OCaml:/home/opam/enum/OCaml \
  -v ${_pwd}/ptrWhy3:/home/opam/enum/ptrWhy3 \
+ -v ${_pwd}/Why3:/home/opam/enum/Why3 \
  --name="enumctr" enum:latest
 
 
